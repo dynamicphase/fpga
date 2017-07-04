c964c3ee13f60700486c43b0e789ee610297b798
dynamicphase <robert.desoto@dynamicphase.com>
2017-07-03 20:33:42 -0500

    broke out functions in verilog_wrapper.py (#1)

--- verilog_wrapper.py
+++ verilog_wrapper.py
@@ -9,97 +9,97 @@ class verilog_wrapper(object):
         self.output_pins = output_pins
         self.inout_pins = inout_pins
 
-    def create_new_file(self):
-        f = open(self.file_name, 'w+')
-        f.write('module  ' + self.module_name + '_wrapper(\n')
+    def connect_port(self, ports, pins):
+        for i,n in enumerate(ports):
+            self.f.write('    .' + n + '(' + pins[i]+ '),\n' )
+
+    def create_instance(self):
+        self.f.write(self.module_name + '    ' + self.module_name + '_inst(\n')
+        self.connect_port(self.input_ports, self.input_pins)
+        self.connect_port(self.output_ports, self.output_pins)
+        self.connect_port(self.inout_ports, self.inout_pins)
+        self.f.write('    );\n')
+        self.f.write('\n')
+
+    def create_top_port(self, n, end):
+        if end == True:
+            self.f.write('    ' + n + ',\n')
+            self.f.write('    ' + n + '_dir\n')
+        else:
+            self.f.write('    ' + n + ',\n')
+            self.f.write('    ' + n + '_dir,\n')
+
+    def create_top_module(self):
+        self.f.write('module  ' + self.module_name + '_wrapper(\n')
         if self.input_pins:
-            for i,n in enumerate(self.input_pins):
-                if i == (len(self.input_pins)-1) and len(self.output_pins) == 0 and len(self.inout_pins) == 0:
-                    f.write('    ' + n + ',\n')
-                    f.write('    ' + n + '_dir\n')
+            for i, n in enumerate(self.input_pins):
+                if i == (len(self.input_pins) - 1) and len(self.output_pins) == 0 and len(self.inout_pins) == 0:
+                    self.create_top_port(n, True)
                 else:
-                    f.write('    ' + n + ',\n')
-                    f.write('    ' + n + '_dir,\n')
+                    self.create_top_port(n, False)
         if self.output_pins:
-            for i,n in enumerate(self.output_pins):
-                if i == (len(self.output_pins)-1) and len(self.inout_pins) == 0:
-                    f.write('    ' + n + ',\n')
-                    f.write('    ' + n + '_dir\n')
-                else:
-                    f.write('    ' + n + ',\n')
-                    f.write('    ' + n + '_dir,\n')
-        if self.inout_pins:
-            for i,n in enumerate(self.inout_pins):
-                if i != (len(self.inout_pins)-1):
-                    f.write('    ' + n + '\n')
-                    f.write('    ' + n + '_dir\n')
-                else:
-                    f.write('    ' + n + ',\n')
-                    f.write('    ' + n + '_dir,\n')
-        f.write('    );\n')
-        f.write('\n')
-
-        if self.input_pins:
-
-            # inout pins for inputs
-            f.write("//Inputs\n")
-            f.write('inout ')
-            for i,n in enumerate(self.input_pins):
-                if i != (len(self.input_pins)-1):
-                    f.write(n + ', ')
-                else:
-                    f.write(n + ';\n')
-            # direction pins for inputs
-            f.write('output ')
-            for i,n in enumerate(self.input_pins):
-                if i != (len(self.input_pins)-1):
-                    f.write(n + '_dir, ')
-                else:
-                    f.write(n + '_dir;\n\n')
-
-        if self.output_pins:
-            # inout pins for outputs
-            f.write('//Outputs\n')
-            f.write('inout ')
-            for i,n in enumerate(self.output_pins):
-                if i != (len(self.output_pins)-1):
-                    f.write(n + ', ')
-                else:
-                    f.write(n + ';\n')
-            # direction pins for outputs
-            f.write('output ')
             for i, n in enumerate(self.output_pins):
-                if i != (len(self.output_pins) - 1):
-                    f.write(n + '_dir, ')
+                if i == (len(self.output_pins) - 1) and len(self.inout_pins) == 0:
+                    self.create_top_port(n, True)
                 else:
-                    f.write(n + '_dir;\n\n')
-
+                    self.create_top_port(n, False)
         if self.inout_pins:
-            f.write('inout ')
             for i, n in enumerate(self.inout_pins):
                 if i != (len(self.inout_pins) - 1):
-                    f.write(n + ', ')
+                    self.create_top_port(n, True)
                 else:
-                    f.write(n + ';\n')
-            # TBD: Need to handle searching for dir pins in top level
+                    self.create_top_port(n, False)
+        self.f.write('    );\n')
+
+    def set_pins(self, dir):
+        self.f.write('inout ')
+        for i, n in enumerate(dir):
+            if i != (len(dir) - 1):
+                self.f.write(n + ', ')
+            else:
+                self.f.write(n + ';\n')
+
+    def set_dir_pins(self, dir):
+        self.f.write('output ')
+        for i, n in enumerate(dir):
+            if i != (len(dir) - 1):
+                self.f.write(n + '_dir, ')
+            else:
+                self.f.write(n + '_dir;\n\n')
+
+    def assign_dir(self, dir, bin):
+        bin_dict = {1 : '1b\'1', 0 : '1b\'0'}
+        for n in dir:
+            self.f.write('assign ' + n + '_dir = ' + bin_dict[bin] + ';\n')
+
+    def create_new_file(self):
+        self.f = open(self.file_name, 'w+')
+        self.create_top_module()
+        self.f.write('\n')
         if self.input_pins:
-            # assign direction pins for inputs to 1b'1
-            for n in self.input_pins:
-                    f.write('assign ' + n + '_dir = 1b\'1;\n')
+            self.f.write("//Inputs\n")
+            self.set_pins(self.input_pins)
+            self.set_dir_pins(self.input_pins)
+
         if self.output_pins:
-            # assign direction pins for outputs to 1b'0
-            for n in self.output_pins:
-                    f.write('assign ' + n + '_dir = 1b\'0;\n')
-        f.write('\n')
-        f.write(self.module_name + '    ' + self.module_name + '_inst(\n')
-        for i,n in enumerate(self.input_ports):
-            f.write('    .' + n + '(' + self.input_pins[i]+ '),\n' )
-        for i,n in enumerate(self.output_ports):
-            f.write('    .' + n + '(' + self.output_pins[i]+ '),\n' )
-        for i,n in enumerate(self.inout_ports):
-            f.write('    .' + n + '(' + self.inout_pins[i]+ '),\n' )
-        f.write('    );\n')
-        f.write('\n')
-        f.write('endmodule')
+            self.f.write('//Outputs\n')
+            self.set_pins(self.output_pins)
+            self.set_dir_pins(self.output_pins)
+
+        if self.inout_pins:
+            self.f.write('//Inouts')
+            self.set_pins(self.inout_pins)
+            # TBD: Need to handle searching for dir pins in top level
+
+        if self.input_pins:
+            # assign direction pins inputs to 1b'1
+            self.assign_dir(self.input_pins, 1)
+        if self.output_pins:
+            # assign direction pins outputs to 1b'0
+            self.assign_dir(self.output_pins, 0)
+
+        self.f.write('\n')
+        self.create_instance()
+        self.f.write('endmodule')
 
 
